91ac6289a200063dbc05821129750084e0e258ca
Alan Mayhew <alanlmayhew@gmail.com>
2017-08-03 19:15:31 -0400

    Changed dwm source URL

--- dwm-setup.sh
+++ dwm-setup.sh
@@ -6,7 +6,7 @@ apt-get update
 apt-get install xorg build-essential libx11-dev libxinerama-dev sharutils
 
 mkdir ~/dwm
-git clone http://github.com/sr/dwm ~/dwm
+git clone http://git.suckless.org/dwm ~/dwm
 cp dwm-things/config.h ~/dwm
 cp -r dwm-things/mystatus ~/dwm
 
