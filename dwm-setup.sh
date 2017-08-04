5ff88578ecf7810291673c337622119f396b1503
Alan Mayhew <alanlmayhew@gmail.com>
2017-08-03 20:04:09 -0400

    Tweaked dwm script to preserve config ownership, and updated README

--- dwm-setup.sh
+++ dwm-setup.sh
@@ -3,11 +3,11 @@
 mkdir wallpapers
 
 apt-get update
-apt-get install xorg build-essential libx11-dev libxinerama-dev sharutils libxft-dev
+apt-get install xorg build-essential libx11-dev libxinerama-dev sharutils libxft-dev suckless-tools
 
 mkdir ~/dwm
 git clone http://git.suckless.org/dwm ~/dwm
-cp dwm-things/config.h ~/dwm
-cp -r dwm-things/mystatus ~/dwm
+cp -p dwm-things/config.h ~/dwm
+cp -rp dwm-things/mystatus ~/dwm
 
 make -C ~/dwm clean install
